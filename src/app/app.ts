e814409d8df1b23ad8ff6cad2d3a2f9be0925470
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-07-09 21:58:36 +0200

    adds todos to todos page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Todo } from './model/todo.type';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class App {
   protected title = 'TodosApp';
+  todos = signal<Array<Todo>>([]);
   newTodo = signal('');
   addTodo() {
     alert('todo added!');
