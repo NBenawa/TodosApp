9ff9b4d1932654b26cc126513f5df48bceae4193
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2025-07-10 22:30:58 +0200

    defines todo service functions

--- src/app/services/todos-service.ts
+++ src/app/services/todos-service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
+
+  http = inject(HttpClient);
+  loadTodosFromApi() {
+    const url = `https://jsonplaceholder.typicode.com/todos`;
+    return this.http.get<Array<Todo>>(url);
+  }
 
   constructor() { }
 }
